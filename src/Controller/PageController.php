8ecb11f4a7ea7674b82bdd6cc76d00a11d81ae5f
Rabillard Tony <tony.rabillard16@gmail.com>
2023-12-13 21:16:53 +0100

    New: Initialization MOVIZ project

---
+++ src/Controller/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $websiteName = 'Moviz';
+        return $this->render('page/index.html.twig', compact('websiteName'));
+    }
+}
